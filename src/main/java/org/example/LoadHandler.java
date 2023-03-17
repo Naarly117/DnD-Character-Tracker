36abefdf3029c1fb619681aad70890fdb2406578
Josh <joshnaar@gmail.com>
2023-03-17 14:13:35 -0400

    Created loadCharacterInitialData() in LoadHandler

--- src/main/java/org/example/LoadHandler.java
+++ src/main/java/org/example/LoadHandler.java
@@ -1,17 +1,49 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class LoadHandler {
 
-    private final String initialPath = "src\\main\\java\\org\\example\\";
+    // Relative path to directory where all character data lives
+    private static final String initialPath = "src\\main\\java\\org\\example\\";
 
     // Returns: DndCharacter object with data loaded in from text files
     public static DndCharacter loadCharacter() {
         System.out.print("Enter name of character: ");
         Scanner input = new Scanner(System.in);
         String name = FormatHandler.normalize(input.nextLine());
-        DndCharacter myCharacter = Main.loadCharacter(name);
+
+        // Create DndCharacter object and load initial character data into it
+        DndCharacter myCharacter = loadCharacterInitialData(name);
+        return myCharacter;
+
+    }
+
+    private static DndCharacter loadCharacterInitialData(String name) {
+        DndCharacter myCharacter = new DndCharacter();
+        try {
+            File inFile = new File(initialPath + name + "\\" + name + ".txt");
+            Scanner myScanner = new Scanner(inFile);
+            TreeMap<String, String> charData = new TreeMap<>();
+            StringBuilder s = new StringBuilder();
+            while (myScanner.hasNextLine()) {
+                String[] temp = myScanner.nextLine().split(" ");
+                for (int i = 1; i < temp.length-1; i++) {
+                    s.append(temp[i]).append(" ");
+                }
+                if (temp.length > 1) {
+                    s.append(temp[temp.length - 1]);
+                }
+                charData.put(temp[0], s.toString());
+                s = new StringBuilder();
+            }
+            myCharacter = new DndCharacter(charData);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unexpected error: loading character data");
+        }
         return myCharacter;
     }
 
